5f929ebad3a84638b737819365ccb3607697c033
DESKTOP-7U5ERIR\boyan.lin <boyan.lin@flowring.com>
2023-05-08 17:07:17 +0800

    feat: 檢查刪除Cache
    調整項目：
    	新增DeleteCache.java，檢查目錄底下檔案，如果存在就刪除掉

---
+++ app/src/main/java/com/flowring/laleents/tools/DeleteCache.java
@@ -0,0 +1,100 @@
+package com.flowring.laleents.tools;
+import com.flowring.laleents.R;
+import com.flowring.laleents.tools.StringUtils;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Environment;
+
+import java.io.File;
+
+
+public class DeleteCache {
+
+    public static void checkExternalSharing(Context context){
+        Resources resources = context.getResources();
+        String app_name = resources.getString(R.string.app_name);
+        String tableOfContents = Environment.DIRECTORY_PICTURES + File.separator + app_name + File.separator + "externalSharing";
+        File download = Environment.getExternalStoragePublicDirectory(tableOfContents);
+        StringUtils.HaoLog("checkExternalSharing " + download);
+        if(download.exists()) {
+            File[] files = download.listFiles();
+            if(files != null && files.length >0){
+                for (File file : files) {
+                    if (file.isFile()) {
+                        String fileName = file.getName();
+                        StringUtils.HaoLog("checkExternalSharing " + fileName);
+                        deleteExternalSharing(file);
+                    }
+                }
+            }else {
+                StringUtils.HaoLog("沒有檔案");
+                return;
+            }
+        }
+    }
+
+    private static void deleteExternalSharing(File file){
+        if(file.isFile()){
+            file.delete();
+        }
+    }
+
+    public static void checkOpenfile(Context context){
+        Resources resources = context.getResources();
+        String app_name = resources.getString(R.string.app_name);
+        String tableOfContents = Environment.DIRECTORY_PICTURES + File.separator + app_name + File.separator + "openfile";
+        File download = Environment.getExternalStoragePublicDirectory(tableOfContents);
+        StringUtils.HaoLog("checkOpenfile " + download);
+        if(download.exists()) {
+            File[] files = download.listFiles();
+            if(files != null && files.length >0){
+                for (File file : files) {
+                    if (file.isFile()) {
+                        String fileName = file.getName();
+                        StringUtils.HaoLog("checkOpenfile " + fileName);
+                        deleteOpenfile(file);
+                    }
+                }
+            } else {
+                StringUtils.HaoLog("沒有檔案");
+                return;
+            }
+        }
+    }
+
+    private static void deleteOpenfile(File file){
+        if(file.isFile()){
+            file.delete();
+        }
+    }
+
+    public static void checkSharefile(Context context){
+        Resources resources = context.getResources();
+        String app_name = resources.getString(R.string.app_name);
+        String tableOfContents = Environment.DIRECTORY_PICTURES + File.separator + app_name + File.separator + "sharefile";
+        File download = Environment.getExternalStoragePublicDirectory(tableOfContents);
+        StringUtils.HaoLog("checkSharefile " + download);
+        if(download.exists()) {
+            File[] files = download.listFiles();
+            if(files != null && files.length >0){
+                for (File file : files) {
+                    if (file.isFile()) {
+                        String fileName = file.getName();
+                        StringUtils.HaoLog("checkSharefile " + fileName);
+                        deleteSharefile(file);
+                    }
+                }
+            }else {
+                StringUtils.HaoLog("沒有檔案");
+                return;
+            }
+        }
+    }
+
+    private static void deleteSharefile(File file){
+        if(file.isFile()){
+            file.delete();
+        }
+    }
+}
